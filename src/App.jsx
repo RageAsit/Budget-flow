a92d6705d5ab6224bf4ac285a922896b47f9210d
RageAsit <asitosahuron@gmail.com>
2025-11-19 23:47:24 +0530

    dhe

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ import {
 // --- 1. API KEYS (PRE-FILLED) ---
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGpFx0dHogy6QppuIm8eO4T5lAmBZtOZc",
+  apiKey: "AIzaSyCGpFX0dHogy6QppuIm8eO4T5lAmBZtOZc",
   authDomain: "budgetflow-c179c.firebaseapp.com",
   projectId: "budgetflow-c179c",
   storageBucket: "budgetflow-c179c.firebasestorage.app",
